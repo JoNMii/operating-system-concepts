9663204d86db3dce0cb1719327effdf16734144a
andrisarkameru <andris.webster@gmail.com>
2015-03-21 14:31:43 +0200

    Removed old ui stuff, Added Ram slider.

--- assets/user-interface.js
+++ assets/user-interface.js
@@ -4,3 +4,23 @@ var slider = new Slider('#ex1', {
 		return 'Current value: ' + value;
 	}
 });
+
+// With JQuery
+$("#ex6").slider();
+$("#ex6").on("slide", function(slideEvt) {
+    var ram = Math.pow(2, slideEvt.value); //KB;
+	$('#ramsize').val(ram);	
+	var label = " KB";
+	if (ram > 2048){
+		label = " MB";
+		ram = ram/1024;
+	}; // Now we MB
+	if (ram > 2048){
+		label = " GB";
+		ram = ram/1024;
+	}; // Now we GB;
+	
+	$("#ex6SliderVal").text(ram+label);
+	
+	
+});
