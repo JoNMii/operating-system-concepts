508c26157cbb31eafb0e08a415d333c0b6218459
Eric Gopak <eric.gopak@gmail.com>
2015-03-21 17:28:20 +0200

    Added LRU sceleton

---
+++ javascript/algorithms/LRU.js
@@ -0,0 +1,24 @@
+var LRU_Algorithm = {
+    "name" : "Least Recently Used",
+    "init" : function() {},
+    "onEvent" : function(event) {
+        var read = function(address) {
+            
+        };
+        
+        var write = function(address) {
+            
+        };
+        
+        switch(event) {
+            case "read":
+                read(event.address);
+                break;
+            case "write":
+                write(event.address);
+                break;
+            default:
+                throw "Unimplemented event type: " + event.type;
+        }
+    }
+}
