7d88f0d6832da3cf816db3baa1ae065b9e6b085e
Valids <valdisxp1@gmail.com>
2015-03-24 12:19:55 +0200

    outline of algo

--- javascript/algorithms/random.js
+++ javascript/algorithms/random.js
@@ -1,24 +1,17 @@
 var randomAlgorithm = {
 	"name":"Pick stuff at random",
 	"onEvent": function(event) {
-        var read = function(address) {
-            
-        };
-        
-        var write = function(address) {
-            
-        };
-        
-        switch(event.type) {
-            case "read":
-                read(event.address);
-                break;
-            case "write":
-                write(event.address);
-                break;
-            default:
-                throw "Unimplemented event type: " + event.type;
-        }
+		var pageId = addressToPageId(event.address);
+		var ramSlot = findRAMPageById(pageId);
+		var swapSlot = findSWAPPageById(pageId);
+		
+		if(ramSlot > -1){
+			//page in RAM nothing to do here
+		} else if((swapSlot <= -1) ){
+			//page is in swap file move it to RAM
+		} else {
+			//page is not realized, create it
+		}
     },
 	"init" : function(){console.log("init called");}
 }
