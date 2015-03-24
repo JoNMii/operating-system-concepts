00942153ffb6a9e149a61718cd90ecb3c51bc0af
Valids <valdisxp1@gmail.com>
2015-03-24 12:46:43 +0200

    this should work

--- javascript/algorithms/random.js
+++ javascript/algorithms/random.js
@@ -2,16 +2,41 @@ var randomAlgorithm = {
 	"name":"Pick stuff at random",
 	"onEvent": function(event) {
 		var pageId = addressToPageId(event.address);
-		var ramSlot = findRAMPageById(pageId);
-		var swapSlot = findSWAPPageById(pageId);
+		var pageInRAM = findRAMPageById(pageId);
+		var pageInSWAP = findSWAPPageById(pageId);
 		
-		if(ramSlot > -1){
-			//page in RAM nothing to do here
-		} else if((swapSlot <= -1) ){
-			//page is in swap file move it to RAM
-		} else {
-			//page is not realized, create it
+		var evictPage = function(){
+			//0 is random tossed a coin, it was heads :D
+			var evicted = 0; //TODO get random slot
+			var swapSlot = getFreeRAMSlot()
+			if(swapSlot<=-1){
+				console.error("OH SHIT IM out of RAM");
+				return -1;
+			}
+			writePageToSwap(getPagesInRAM()[evicted],swapSlot);
+			
+			return evicted;
 		}
+		
+		if(pageInRAM){
+			//page in RAM
+			//nothing to do here
+		} else 
+			if(pageInSWAP){
+				//page is in swap file move it to RAM
+				var target = getFreeRAMSlot();
+				if(target <= -1){
+					target = evictPage();
+				}
+				writePageToRAM(pageInSWAP,target);
+			} else {
+				//page is not realized, create it
+				var target = getFreeRAMSlot();
+				if(target <= -1){
+					target = evictPage();
+				}
+				createPage(target, pageId);
+			}
     },
 	"init" : function(){console.log("init called");}
 }
