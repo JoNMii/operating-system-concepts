bf844f719ecc7c5ed980285355e826bab0216e56
Valdis <valdisxp1@gmail.com>
2015-03-26 11:04:09 +0200

    page numbers must be integers

--- javascript/algorithms/random.js
+++ javascript/algorithms/random.js
@@ -1,7 +1,7 @@
 var randomAlgorithm = function(){
 	var simple = simpleAlgorithm();
 	simple.onEvict = function(){
-		return Math.random*pageSlotsInRAM();
+		return Math.floor(Math.random*pageSlotsInRAM());
 	}
 	simple.name="Random";
 	return simple;
