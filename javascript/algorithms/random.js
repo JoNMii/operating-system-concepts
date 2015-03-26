3d08a9fe266b6aed90358748c4898b49b1aaf2a2
Valdis <valdisxp1@gmail.com>
2015-03-26 10:49:54 +0200

    refactored, extract common algo components

--- javascript/algorithms/random.js
+++ javascript/algorithms/random.js
@@ -1,43 +1,7 @@
-var randomAlgorithm = {
-	"name":"Pick stuff at random",
-	"onEvent": function(event) {
-		var pageId = addressToPageId(event.address);
-		var pageInRAM = findRAMPageById(pageId);
-		var pageInSWAP = findSWAPPageById(pageId);
-		
-		var evictPage = function(){
-			var totalPages = pageSlotsInRAM();
-			var evicted = Math.random*pageSlotsInRAM();
-			var swapSlot = getFreeRAMSlot()
-			if(swapSlot<=-1){
-				console.error("OH SHIT IM out of RAM");
-				return -1;
-			}
-			writePageToSwap(getPagesInRAM()[evicted],swapSlot);
-			
-			return evicted;
-		}
-		
-		if(pageInRAM){
-			//page in RAM
-			//nothing to do here
-			pageHit(pageId);
-		} else 
-			if(pageInSWAP){
-				//page is in swap file move it to RAM
-				var target = getFreeRAMSlot();
-				if(target <= -1){
-					target = evictPage();
-				}
-				writePageToRAM(pageInSWAP,target);
-			} else {
-				//page is not realized, create it
-				var target = getFreeRAMSlot();
-				if(target <= -1){
-					target = evictPage();
-				}
-				createPage(target, pageId);
-			}
-    },
-	"init" : function(){console.log("init called");}
-}
+var randomAlgorithm = function(){
+	var simple = simpleAlgorithm();
+	simple.onEvict = function(){
+		return Math.random*pageSlotsInRAM();
+	}
+	return simple;
+}();
---
+++ javascript/algorithms/simple.js
@@ -0,0 +1,49 @@
+function simpleAlgorithm() {
+return {
+	"name":"Name me",
+	"onEvict":function(){
+		console.log("Implement me!!!");
+		return 0;
+	},
+	"onEvent": function(event) {
+		var pageId = addressToPageId(event.address);
+		var pageInRAM = findRAMPageById(pageId);
+		var pageInSWAP = findSWAPPageById(pageId);
+		
+		var evictPage = function(){
+			var totalPages = pageSlotsInRAM();
+			var evicted = this.onEvict();
+			var swapSlot = getFreeRAMSlot()
+			if(swapSlot<=-1){
+				console.error("OH SHIT IM out of RAM");
+				return -1;
+			}
+			writePageToSwap(getPagesInRAM()[evicted],swapSlot);
+			
+			return evicted;
+		}
+		
+		if(pageInRAM){
+			//page in RAM
+			//nothing to do here
+			pageHit(pageId);
+		} else 
+			if(pageInSWAP){
+				//page is in swap file move it to RAM
+				var target = getFreeRAMSlot();
+				if(target <= -1){
+					target = evictPage();
+				}
+				writePageToRAM(pageInSWAP,target);
+			} else {
+				//page is not realized, create it
+				var target = getFreeRAMSlot();
+				if(target <= -1){
+					target = evictPage();
+				}
+				createPage(target, pageId);
+			}
+    },
+	"init" : function(){console.log("init called");}
+};
+}
