84d0411415be39af2d15fd8de33ce06f6813eb74
Valters Gajevskis <valtersg2@gmail.com>
2015-03-27 10:58:50 +0200

    Simple SC algo

--- javascript/algorithms/second_chance.js
+++ javascript/algorithms/second_chance.js
@@ -1,38 +1,48 @@
-var second_chance = {
-    var eventList = [];
+var eventlist = [];
+
+var second_chance = function(){
 	var simple = new simpleAlgorithm();
 	simple.name = "Second Chance";
 	var defaultOnEvent = simple.onEvent;
 	simple.onEvict = function() {
 	    while(true) {
-	        var flags = getFlags(eventList[0]);
-	        if (flags.count > 0) {
-	            var tmp = eventList[0];
-	            eventList.shift();
-	            eventList.push(tmp);
+	        var flags = getFlags(eventlist[0]);
+	        if (flags && flags.count > 0) {
+	            var tmp = eventlist[0];
+	            eventlist.shift();
+	            eventlist.push(tmp);
 	            flags.count = 0;
 	            setFlags(tmp, flags);
 	        } else {
-	            var tmp = eventList[0];
-	            eventList.shift();
-	            return tmp;
+	            var tmp = eventlist[0];
+	            eventlist.shift();
+	            return findRAMPageById(tmp);
 	        }
 	    }
 	}
 	simple.onEvent = function(event) {
 	    var pageId = addressToPageId(event.address);
-	    eventList.push(pageId);
-	    var flags = getFlags(pageId);
-	    if (flags.count) {
-	        flags.count += 1;
-	    } else {
-	        flags.count = 0;
+	    if (eventlist.indexOf(pageId) == -1) {
+	    	eventlist.push(pageId);
 	    }
+        var flags = getFlags(pageId);
+        if (!flags) {
+            flags = {"count": 0};
+        } else if (!flags.count) {
+            flags.count = 0;
+        } else {
+            flags.count += 1;
+        }
 	    if (event.type == "write") {
 	        flags.dirty = true;
 	    }
 	    defaultOnEvent(event);
 	    setFlags(pageId, flags);
+	    var tmp = getFlags(pageId);
 	}
-
+    simple.init = function() {
+        console.log("init called");
+        eventlist = [];
+    }
+    return simple;
 }();
