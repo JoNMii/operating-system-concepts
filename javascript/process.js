ea38de52e53bbbdef717076fda0f69ef5ff76142
Valters Gajevskis <valtersg2@gmail.com>
2015-04-02 18:04:06 +0300

    Starting pagetoframe

--- javascript/process.js
+++ javascript/process.js
@@ -1,5 +1,33 @@
+var pageToFrame = {
+    pageTable: {}, //{pageid: [ramSlot, V or I]}
+    init: function() {
+        this.pageTable = {}; //Clear table
+        //Set up page table
+        for (var i=0; i<config.virualMemorySize/config.frameSize; i++) {
+            this.pageTable[i] = [-1, "I"]; //All entries invalid
+        };
+    },
+    getFrame: function(pageId) {
+        if (pageId >= (config.virualMemorySize/config.frameSize) || pageId < 0) {
+            console.log("Invalid page id: ",pageId);
+            return -1;
+        } else if (this.pageTable[pageId][1] == "I") {
+            //Read page into Ram
+            //Use algo to find witch frame to swap out
+            //Change swapped out to I
+            //Write address into table and change from I to V
+            //Return ramslot
+            return this.pageTable[pageId][0];
+        } else {
+            //Page already in Ram, return ramSlot
+            return this.pageTable[pageId][0];
+        };
+    },
+};
+
+
 function process() {
-    this.pageList = []; //Pages available for process
+    this.pageList = []; //virtual pages available for process
     this.maxPageCount = 0;
     this.createPage = function() {
         //TODO: Implement page creation
