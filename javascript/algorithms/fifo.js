74120ca3b836db302c0a06e734e2cdffbc31646b
Valdis Adamsons <valdisxp1@gmail.com>
2015-04-04 13:49:15 +0300

    fifo added

--- javascript/algorithms/fifo.js
+++ javascript/algorithms/fifo.js
@@ -1,37 +1,37 @@
 var FIFO_Algorithm = function(){
-
+	var turn = 0;
 	var simple = new simpleAlgorithm();
 	simple.name="FIFO";
-
-	simple.onEvict = function(){ 	//finds the page to swap out
+	simple.onEvict = function(){
 		var pages = getPagesInRAM();
 		var oldestPage = pages[0];
-		return pageId(oldestPage);
+		var oldest = getFlags(oldestPage).created;
+		var result = 0;
+		for(i in pages){
+			var created = getFlags(pages[i]).created;
+			console.log("Created on",created, pages[i]);
+			if(created === undefined){
+				return parseInt(i);
+			}
+			if(oldest>created){
+				oldest = created;
+				oldestPage = pages[i];
+				result = parseInt(i);
+			}
+		}
+		return result;
 	}
-
 	var defaultOnEvent = simple.onEvent;
-
 	simple.onEvent=function(event){
-		
-		defaultOnEvent(event);	//rekursija??
-		
+		turn++;
 		var pageId = addressToPageId(event.address);
-
-		if (eventlist.indexOf(pageId) == -1) {
-            eventlist.push(pageId);
-        }
-
-        var flags = getFlags(pageId);
-
-        if (event.type == "write") {
-        	if (!flags) {
-            flags = {"dirty": true};
-        	} else {
-        	    flags.dirty = true;
-        	}
-	    }
-	
-	    setFlags(pageId, flags);
+		var existed = (findRAMPageById()!=null) || (findSWAPPageById()!=null);
+		defaultOnEvent(event);
+		// if it did not exists, then it is created
+		if(!existed){
+			console.log("Create detected"+pageId);
+			setFlags(pageId,{"created":turn});
+		}
 	}
 
 	return simple;
